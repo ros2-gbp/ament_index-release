53a9926267cde70fe5c8c4622a2b8cd873c9f99c
Scott K Logan <logans@cottsay.net>
2025-04-17 18:51:32 -0500

    Rebase from 'upstream'

--- ament_index_python/setup.py
+++ ament_index_python/setup.py
@@ -5,7 +5,7 @@ package_name = 'ament_index_python'
 
 setup(
     name=package_name,
-    version='1.10.1',
+    version='1.10.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -16,6 +16,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
     ],
+    package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     zip_safe=True,
     author='Dirk Thomas',
