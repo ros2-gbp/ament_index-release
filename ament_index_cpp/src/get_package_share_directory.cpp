aa9892e0cbd09ee26c97494af634ac5fb2d3e621
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-02-25 13:47:58 +0100

    Revert "Trimmed the branch to only the ament_index_python sub directory"
    This reverts commit 9346fe689dce9144027b6c2ec2c47c38deab0a6e.

---
+++ ament_index_cpp/src/get_package_share_directory.cpp
@@ -0,0 +1,42 @@
+// Copyright 2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
+#include <filesystem>
+#include <string>
+
+#include "ament_index_cpp/get_package_prefix.hpp"
+
+namespace ament_index_cpp
+{
+
+std::string
+get_package_share_directory(const std::string & package_name)
+{
+  std::filesystem::path result;
+  get_package_share_directory(package_name, result);
+  return result.string();
+}
+
+void get_package_share_directory(
+  const std::string & package_name,
+  std::filesystem::path & path)
+{
+  std::filesystem::path result;
+  get_package_prefix(package_name, result);
+  path = result / "share" / package_name;
+}
+
+}  // namespace ament_index_cpp
